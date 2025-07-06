1a947f4c59c0e6ab132010339895eecd06649b9b
Deep100ni <deep100ni13@gmail.com>
2025-07-06 23:47:54 +0530

    Remove Linked List Elements

---
+++ src/main/kotlin/removeLinkedListElements.kt
@@ -0,0 +1,17 @@
+package org.example
+
+class Solution2 {
+    fun addTwoNumbers(head : ListNode,num : Int): ListNode {
+        var p1: ListNode? = head
+        var resultHead: ListNode? = null
+        while (p1 != null){
+            if (p1. == num){
+                p1 = p1.next
+            }else{
+                resultHead = p1
+            }
+        }
+        return resultHead
+    }
+
+}
