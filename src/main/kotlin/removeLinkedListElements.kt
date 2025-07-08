45547977cf0338672be219d777bbfa4b87283848
Deep100ni <deep100ni13@gmail.com>
2025-07-08 23:51:29 +0530

    Remove Linked List Elements v1

--- src/main/kotlin/removeLinkedListElements.kt
+++ src/main/kotlin/removeLinkedListElements.kt
@@ -1,12 +1,12 @@
 package org.example
 
 class Solution2 {
-    fun addTwoNumbers(head : ListNode,num : Int): ListNode? {
+    fun addTwoNumbers(head: ListNode?, num: Int): ListNode? {
         var p1: ListNode? = head
         var resultHead: ListNode? = null
         while (p1 != null){
             if (p1.`val` == num){
-                p1 = p1.next!!
+               p1 = p1.next!!
             }else{
                 resultHead = p1
             }
@@ -17,11 +17,23 @@ class Solution2 {
 }
 
 fun main(){
-    val l1 = convertToLinkedList(76198716)
+    val l1 = convertToLinkedList1(76198716)
     val num = 1
     val solution = Solution2()
     val result = solution.addTwoNumbers(l1,num)
     println(l1)
     println("Result: $result")
 }
+
+fun convertToLinkedList1(number: Int): ListNode? {
+    val digits = number.toString().map { it - '0' }
+    val dummy = ListNode(0)
+    var current = dummy
+    for (d in digits) {
+        current.next = ListNode(d)
+        current = current.next!!
+    }
+    return dummy.next
+}
+
 
