c20d4e8840ea90263136f2555766fbc127c85dfc
Deep100ni <deep100ni13@gmail.com>
2025-07-10 17:42:04 +0530

    Remove Linked List Elements v2

--- src/main/kotlin/removeLinkedListElements.kt
+++ src/main/kotlin/removeLinkedListElements.kt
@@ -1,39 +1,43 @@
 package org.example
 
 class Solution2 {
-    fun addTwoNumbers(head: ListNode?, num: Int): ListNode? {
-        var p1: ListNode? = head
-        var resultHead: ListNode? = null
-        while (p1 != null){
-            if (p1.`val` == num){
-               p1 = p1.next!!
-            }else{
-                resultHead = p1
+    fun removeLinkedListElements(head: ListNode?, num: Int): ListNode? {
+        val linkedList = ListNode(0)
+        linkedList.next = head
+        var current = linkedList
+
+        while (current.next != null) {
+            if (current.next!!.`val` == num) {
+                current.next = current.next!!.next
+            } else {
+                current = current.next!!
             }
         }
-        return resultHead
-    }
 
+        return linkedList.next
+    }
 }
+
 
 fun main(){
     val l1 = convertToLinkedList1(76198716)
     val num = 1
-    val solution = Solution2()
-    val result = solution.addTwoNumbers(l1,num)
     println(l1)
+    val solution = Solution2()
+    val result = solution.removeLinkedListElements(l1,num)
     println("Result: $result")
 }
 
-fun convertToLinkedList1(number: Int): ListNode? {
-    val digits = number.toString().map { it - '0' }
-    val dummy = ListNode(0)
-    var current = dummy
-    for (d in digits) {
-        current.next = ListNode(d)
-        current = current.next!!
+fun convertToLinkedList1(num: Int): ListNode {
+    val digits = num.toString().map { it - '0' }
+    val head = ListNode(digits[0])
+    var current = head
+
+    for (i in 1 until digits.size) {
+        val node = ListNode(digits[i])
+        current.next = node
+        current = node
     }
-    return dummy.next
+
+    return head
 }
-
-
