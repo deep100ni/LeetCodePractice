2911581c547f5f2a5e125d7d4664a031e938fc81
Deep100ni <deep100ni13@gmail.com>
2025-07-13 22:00:43 +0530

    replace linkedlist

--- src/main/kotlin/validParenthisies.kt
+++ src/main/kotlin/validParenthisies.kt
@@ -1,4 +1,23 @@
 package org.example
 
-class validParenthisies {
+fun isValid(s: String): Boolean {
+    var valid = true
+    for (t in s){
+        if(t in listOf('(','[','{')){
+            for (l in s){
+                if (l in listOf(')',']','}')){
+                    valid = true
+                }else{
+                    valid = false
+                }
+            }
+        }else{
+            valid = true
+        }
+    }
+    return valid
+}
+fun main(){
+    val s = "[][}"
+    println(isValid(s))
 }
