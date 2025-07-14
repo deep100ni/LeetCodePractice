79e1587df5601c8517d3239b5484250fe6c422b9
Deep100ni <deep100ni13@gmail.com>
2025-07-14 22:35:49 +0530

    replace linkedlist

--- src/main/kotlin/validParenthisies.kt
+++ src/main/kotlin/validParenthisies.kt
@@ -2,22 +2,28 @@ package org.example
 
 fun isValid(s: String): Boolean {
     var valid = true
-    for (t in s){
-        if(t in listOf('(','[','{')){
-            for (l in s){
-                if (l in listOf(')',']','}')){
-                    valid = true
-                }else{
-                    valid = false
-                }
-            }
+    var  stb = mutableListOf<Char>()
+    for (b in s){
+        if (b in listOf('(','[','{')){
+            stb.add(b)
         }else{
-            valid = true
+            if ((b == ')' && stb.last() == '(') || (b == ']' &&  stb.last() == '[') || (b == '}' && stb.last() == '{')){
+                stb.remove(stb.last())
+            }else{
+                valid = false
+                stb.add(b)
+                break;
+            }
         }
+    }
+    if(stb.size == 0){
+        valid = true
+    }else{
+        valid = false
     }
     return valid
 }
 fun main(){
-    val s = "[][}"
+    val s = "[({}){}()]"
     println(isValid(s))
 }
