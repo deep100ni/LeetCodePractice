258d145cfedc4e47720c312aa8ba037e27d557a1
Deep100ni <deep100ni13@gmail.com>
2025-07-17 18:38:47 +0530

    Remove Element

--- src/main/kotlin/removeElement.kt
+++ src/main/kotlin/removeElement.kt
@@ -4,6 +4,7 @@ fun removeElement(nums: IntArray, `val`: Int): Int {
     var k = 0
     for(i in nums.indices){
         if (nums[i] != `val`){
+            nums[k] = nums[i]
             k++
         }
     }
