f0df03b1e25ac6e48574f864960cb65115b401a5
Deep100ni <deep100ni13@gmail.com>
2025-07-17 18:36:30 +0530

    Remove Element

---
+++ src/main/kotlin/removeElement.kt
@@ -0,0 +1,17 @@
+package org.example
+
+fun removeElement(nums: IntArray, `val`: Int): Int {
+    var k = 0
+    for(i in nums.indices){
+        if (nums[i] != `val`){
+            k++
+        }
+    }
+    return k
+}
+
+fun main(){
+    val nums = intArrayOf(3,2,2,3)
+    val e = 2
+    println(removeElement(nums,e))
+}
