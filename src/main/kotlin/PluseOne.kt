14363a383dec0182f7dabd42f253d55fee7725a6
Deep100ni <deep100ni13@gmail.com>
2025-07-20 14:28:33 +0530

    Pluse One

--- src/main/kotlin/PluseOne.kt
+++ src/main/kotlin/PluseOne.kt
@@ -1,13 +1,5 @@
 package org.example
 
 fun plusOne(digits: IntArray): IntArray {
-    var lastElement = digits.last()
-    var duplicate = lastElement
-    if (lastElement >= 9){
-        lastElement.plus(lastElement/10)
-        lastElement.plus(duplicate % 10)
-    }else{
-        lastElement++
-    }
-    return digits
+
 }
