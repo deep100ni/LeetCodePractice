ed14ea59e85f8aa7f8b7dce4e1c3b66b514026a4
Deep100ni <deep100ni13@gmail.com>
2025-07-20 15:11:41 +0530

    Pluse One v1

--- src/main/kotlin/PluseOne.kt
+++ src/main/kotlin/PluseOne.kt
@@ -1,5 +1,16 @@
 package org.example
 
 fun plusOne(digits: IntArray): IntArray {
+    var resNum = intArrayToNumber(digits)
+    resNum = resNum + 1
+    var resArr = numberToIntArray(resNum)
+    return resArr
+}
 
+fun numberToIntArray(num: Int): IntArray {
+    return num.toString().map { it.toString().toInt() }.toIntArray()
+}
+
+fun intArrayToNumber(arr: IntArray): Int {
+    return arr.joinToString("").toInt()
 }
